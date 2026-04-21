5f2a1ed1a6e569bfbbcca4a057652148a2b1e506
Puru <232665682+PuruPuru71@users.noreply.github.com>
2026-04-21 13:50:41 +0200

    basic runtime_error handling

--- decode.cpp
+++ decode.cpp
@@ -24,6 +24,7 @@ decode::~decode()
 void decode::on_decodeBtn_clicked()
 {
     // This is 99% implemented for now because if hash doesn't have ":" in it, it will put the same value in both fields.
+    try {
     hashstr = ui->hashField->text().toStdString();
     hashstrdec = base64::from_base64(hashstr);
     stringstream hash_ss(hashstrdec);
@@ -42,7 +43,11 @@ void decode::on_decodeBtn_clicked()
         ui->usernameField->setText(user_qstr);
         ui->passwordField->setText(pwd_qstr);
     }
-
+    }
+    catch (std::runtime_error& e) {
+            ui->usernameField->setText("");
+            ui->passwordField->setText("");
+    }
 }
 
 
