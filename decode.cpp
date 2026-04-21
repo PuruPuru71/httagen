1700f5f01065de9b8ca44455b4b40b453f0ebd00
Puru <232665682+PuruPuru71@users.noreply.github.com>
2026-04-21 12:16:10 +0200

    implement mostly functional decoder

--- decode.cpp
+++ decode.cpp
@@ -2,6 +2,9 @@
 #include "ui_decode.h"
 #include "base64.hpp"
 #include "about.h"
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 decode::decode(QWidget *parent)
@@ -20,14 +23,24 @@ decode::~decode()
 
 void decode::on_decodeBtn_clicked()
 {
-    // This is 95% implemented because for now this is a Generic base64 decoder, no matter if it's a HTTP Authorizaation Token or not.
+    // This is 99% implemented for now because if hash doesn't have ":" in it, it will put the same value in both fields.
     hashstr = ui->hashField->text().toStdString();
     hashstrdec = base64::from_base64(hashstr);
+    stringstream hash_ss(hashstrdec);
+    vector<string> v;
     if (hashstr == "") {
-        ui->resultField->setText("");
+        ui->usernameField->setText("");
+        ui->passwordField->setText("");
     }
     else {
-        ui->resultField->setText(QString::fromStdString(hashstrdec));
+        // There has to be a better way to do this...
+        int split_userpwd = hashstrdec.find_first_of(':');
+        string user = hashstrdec.substr(0, split_userpwd);
+        string pwd = hashstrdec.substr(split_userpwd+1);
+        QString user_qstr = QString::fromStdString(user);
+        QString pwd_qstr = QString::fromStdString(pwd);
+        ui->usernameField->setText(user_qstr);
+        ui->passwordField->setText(pwd_qstr);
     }
 
 }
